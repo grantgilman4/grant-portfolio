a734bd6f0085605351deddacf02c68f6a9f64e6f
Grant Gilman <grantgilman4@gmail.com>
2023-09-05 11:26:26 -0400

    resume update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
                   </Link>
                 )
               })}
-              <a href="https://docs.google.com/document/d/e/2PACX-1vQkEbn4GfXAoQGJTn49JhBi_nfCA0sObnoA0PLQTN4xD_zpkmiu4YMcWtpmuZQjAQ/pub" rel='noreferrer' target='_blank'>Resume</a>
+              <a href="https://docs.google.com/document/d/1ZkXsB5HZ99X_fQdEwppbLU8dSBmfyXr4/edit?usp=sharing&ouid=111881514041784590831&rtpof=true&sd=true" rel='noreferrer' target='_blank'>Resume</a>
               {currentTheme === "dark" ? (
                 <button
                   onClick={() => setTheme("light")}
